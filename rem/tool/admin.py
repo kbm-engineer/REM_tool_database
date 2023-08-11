14faf17c28e4fa2fa05b50c9bc556bf2b7da278e
Boris Kirillov <kirillov_boris_92@mail.ru>
2023-08-11 17:53:46 +0300

    обновил requirements.txt

--- rem/tool/admin.py
+++ rem/tool/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from .models import ViewEquipment, TypeEquipment, BaseMonitoringObject
+from .models import ViewEquipment, TypeEquipment, BaseMonitoringObject, UniqueIdentificator
 from django.db import models
+
+
+@admin.register(UniqueIdentificator)
+class UniqueIdentificatorAdmin(admin.ModelAdmin):
+    pass
 
 @admin.register(ViewEquipment)
 class ViewEquipmentAdmin(admin.ModelAdmin):
--- rem/tool/models.py
+++ rem/tool/models.py
@@ -1,5 +1,14 @@
+from typing import Iterable, Optional
 from django.db import models
 from smart_selects.db_fields import ChainedForeignKey
+import time
+
+class UniqueIdentificator(models.Model):
+    nomber = models.TextField(max_length=16, unique=True, blank=True)
+
+    def fill_nomber(self, instance, **kwargs):
+        instance.nomber = str(time.time())
+
 
 class ViewEquipment(models.Model):
     name = models.CharField(max_length=100, unique=True)
@@ -15,6 +24,7 @@ class TypeEquipment(models.Model):
         return self.name
 
 class BaseMonitoringObject(models.Model):
+    # id_nomber = 
     name = models.CharField(max_length=100, unique=True)
     type = models.ForeignKey(TypeEquipment, on_delete=models.CASCADE)
 
